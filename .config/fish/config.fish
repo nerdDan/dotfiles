7101087f67b83c8166eacd3fdac6e1e2c1c1544b
daniel <danichau93@gmail.com>
2020-06-17 12:55:01 +0800

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,14 @@
-set -x _JAVA_AWT_WM_NONREPARENTING 1
 set -x EDITOR nvim
 set -x GTK_IM_MODULE fcitx
+set -x MOZ_ENABLE_WAYLAND 1
 set -x QT_AUTO_SCREEN_SCALE_FACTOR 0
 set -x QT_IM_MODULE fcitx
-set -x SONAR_SCANNER_HOME /opt/sonar-scanner
 set -x XMODIFIERS @im=fcitx
+set -x _JAVA_AWT_WM_NONREPARENTING 1
 
-set -x PATH $PATH $HOME/.gem/ruby/2.6.0/bin
 set -x PATH $PATH $HOME/.local/bin
+set -x PATH $PATH $HOME/.gem/ruby/2.7.0/bin
 set -x PATH $PATH $HOME/.yarn/bin
-set -x PATH $PATH $SONAR_SCANNER_HOME/bin
 
 kitty + complete setup fish | source
 
